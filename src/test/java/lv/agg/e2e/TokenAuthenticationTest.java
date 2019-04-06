4ac382a150073d1c89f3b5dd62fc3cf092a81318
Leha <avoroncovs@gmail.com>
2019-04-06 21:25:00 +0300

    token authentication integration test

--- src/test/java/lv/agg/e2e/TokenAuthenticationTest.java
+++ src/test/java/lv/agg/e2e/TokenAuthenticationTest.java
@@ -1,11 +1,16 @@
 package lv.agg.e2e;
 
 import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
 import lv.agg.dto.JwtAuthenticationResponse;
 import lv.agg.dto.ServiceDTO;
+import lv.agg.dto.UserProfileDTO;
+import lv.agg.entity.UserEntity;
+import lv.agg.repository.UserRepository;
 import org.awaitility.Awaitility;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -13,8 +18,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.util.concurrent.TimeUnit;
 
 import static com.jayway.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
 
 @SpringBootTest(
@@ -30,11 +34,111 @@ public class TokenAuthenticationTest {
         RestAssured.port = port;
     }
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void testRegisterAsIndividualMerchant() {
+        given().when()
+                .contentType(ContentType.JSON)
+                .body(prepareUserProfileDTO("a1@aa.com", "12345", "12345", UserEntity.UserRole.ROLE_MERCHANT, false, "John", "Doe", "blabla", "Wall St. 1"))
+                .post("api/v1/user")
+                .then().statusCode(201);
+        testMerchantLogin("a1@aa.com", "12345");
+        UserEntity userEntity = userRepository.findByEmail("a1@aa.com").orElseThrow(RuntimeException::new);
+        assertThat(userEntity.getUserRole(), is(UserEntity.UserRole.ROLE_MERCHANT));
+        assertThat(userEntity.getMerchantType(), is(UserEntity.MerchantType.INDIVIDUAL));
+        assertThat(userEntity.getCompanyName(), is(nullValue()));
+        assertThat(userEntity.getFirstName(), is("John"));
+        assertThat(userEntity.getLastName(), is("Doe"));
+        assertThat(userEntity.getAddress(), is("Wall St. 1"));
+    }
+
+    @Test
+    public void testRegisterAsCompanyMerchant() {
+        given().when()
+                .contentType(ContentType.JSON)
+                .body(prepareUserProfileDTO("a2@aa.com", "12345", "12345", UserEntity.UserRole.ROLE_MERCHANT, true, "John", "Doe", "company ltd", "Wall St. 1"))
+                .post("api/v1/user")
+                .then().statusCode(201);
+        testMerchantLogin("a2@aa.com", "12345");
+        UserEntity userEntity = userRepository.findByEmail("a2@aa.com").orElseThrow(RuntimeException::new);
+        assertThat(userEntity.getUserRole(), is(UserEntity.UserRole.ROLE_MERCHANT));
+        assertThat(userEntity.getMerchantType(), is(UserEntity.MerchantType.COMPANY));
+        assertThat(userEntity.getCompanyName(), is("company ltd"));
+        assertThat(userEntity.getFirstName(), is("John"));
+        assertThat(userEntity.getLastName(), is("Doe"));
+        assertThat(userEntity.getAddress(), is("Wall St. 1"));
+    }
+
+    @Test
+    public void testRegisterAsCustomer() {
+        given().when()
+                .contentType(ContentType.JSON)
+                .body(prepareUserProfileDTO("a3@aa.com", "12345", "12345", UserEntity.UserRole.ROLE_CUSTOMER, true, "John", "Doe", "company ltd", "Wall St. 1"))
+                .post("api/v1/user")
+                .then().statusCode(201);
+        testMerchantLogin("a3@aa.com", "12345");
+        UserEntity userEntity = userRepository.findByEmail("a3@aa.com").orElseThrow(RuntimeException::new);
+        assertThat(userEntity.getUserRole(), is(UserEntity.UserRole.ROLE_CUSTOMER));
+        assertThat(userEntity.getMerchantType(), is(nullValue()));
+        assertThat(userEntity.getCompanyName(), is(nullValue()));
+        assertThat(userEntity.getFirstName(), is("John"));
+        assertThat(userEntity.getLastName(), is("Doe"));
+        assertThat(userEntity.getAddress(), is("Wall St. 1"));
+    }
+
+    private void testMerchantLogin(String username, String password) {
+        given().when()
+                .param("username", username)
+                .param("password", "wrong_password")
+                .post("api/v1/user/login")
+                .then().statusCode(401);
+
+        JwtAuthenticationResponse jwtAuthenticationResponse = given().when()
+                .param("username", username)
+                .param("password", password)
+                .post("api/v1/user/login")
+                .then().statusCode(200)
+                .extract().as(JwtAuthenticationResponse.class);
+        String accessToken = jwtAuthenticationResponse.getAccessToken();
+        String refreshToken = jwtAuthenticationResponse.getRefreshToken();
+
+        assertThat(accessToken, is(notNullValue()));
+        assertThat(refreshToken, is(notNullValue()));
+
+        ServiceDTO[] services = given().header("Authorization", "Bearer " + accessToken).when().get("api/v1/service")
+                .then().statusCode(200)
+                .extract().as(ServiceDTO[].class);
+        assertThat(services.length, is(1));
+        assertThat(services[0].getName(), is("haircut"));
+    }
+
+    private UserProfileDTO prepareUserProfileDTO(String email, String password, String passwordRepeat,
+                                                 UserEntity.UserRole userRole, boolean isCompany, String firstName,
+                                                 String lastName, String companyName, String address) {
+        UserProfileDTO dto = new UserProfileDTO();
+        dto.setEmail(email);
+        dto.setPassword(password);
+        dto.setPasswordRepeat(passwordRepeat);
+        dto.setUserRole(userRole);
+        if (userRole == UserEntity.UserRole.ROLE_MERCHANT) {
+            dto.setIsCompany(isCompany);
+            if (isCompany) {
+                dto.setCompanyName(companyName);
+            }
+        }
+        dto.setFirstName(firstName);
+        dto.setLastName(lastName);
+        dto.setAddress(address);
+        return dto;
+    }
+
     @Test
     public void testFailToGetLoginThenGetThenWaitThenFailToGetThenRefreshTokenThenGet() {
         given().when().get("api/v1/service").then().statusCode(401);
         JwtAuthenticationResponse jwtAuthenticationResponse = given().when()
-                .param("username", "service_provider@mail.com")
+                .param("username", "merchant@mail.com")
                 .param("password", "123")
                 .post("api/v1/user/login")
                 .then().statusCode(200)
@@ -70,6 +174,5 @@ public class TokenAuthenticationTest {
                 .extract().as(ServiceDTO[].class);
         assertThat(services.length, is(1));
         assertThat(services[0].getName(), is("haircut"));
-
     }
 }
